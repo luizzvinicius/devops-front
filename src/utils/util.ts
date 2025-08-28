94b23e93b5772587533cd3afa23e5b815970e9e1
luiz <viniciusadr244@gmail.com>
2025-08-27 21:16:01 -0300

    feat: showDateFormatted function

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,3 +1,9 @@
+import { format } from "date-fns";
+
 export function showCpfFormatted(cpf: string) {
-    return `${cpf.substring(0, 3)}.${cpf.substring(3, 6)}.${cpf.substring(6, 9)}-${cpf.substring(9, 11)}`
+	return `${cpf.substring(0, 3)}.${cpf.substring(3, 6)}.${cpf.substring(6, 9)}-${cpf.substring(9, 11)}`;
+}
+
+export function showDateFormatted(date: Date) {
+	return format(date, "dd/MM/yyyy");
 }
