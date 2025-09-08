fc27cbcb3bc09895f70941620063b679808201b4
luiz <viniciusadr244@gmail.com>
2025-09-08 20:56:51 -0300

    refactor: update docker-compose url

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -4,9 +4,3 @@ export const {
 	BASE_URL = `http://backend:${PORT}/${API_SUFFIX}`,
 	DEV_MODE = process.env.NODE_ENV === "development",
 } = process.env;
-// export const {
-//     PORT = 8080,
-//     API_SUFFIX = "api/v1",
-//     BASE_URL = `http://localhost:${PORT}/${API_SUFFIX}`,
-//     DEV_MODE = process.env.NODE_ENV === "development",
-// } = process.env;
