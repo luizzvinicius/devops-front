263a8b69030f44a029a5714a9e2da7bda9c734aa
luiz <viniciusadr244@gmail.com>
2025-09-09 19:30:10 -0300

    refactor: remove redirect function

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	// decrease the size of the docker image
-	output: 'standalone',
-	// biome-ignore lint/suspicious/useAwait: <explanation>
-	async redirects() {
-		return [
-			{
-				source: "/",
-				destination: "/pessoa",
-				permanent: true,
-			},
-		];
-	},
+	output: "standalone",
 };
 
 export default nextConfig;
