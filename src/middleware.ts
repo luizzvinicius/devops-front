82bc1069ecf5d1cac7e30882149b1d72e56824da
luiz <viniciusadr244@gmail.com>
2025-09-19 21:12:24 -0300

    refactor: remove middleware console.log

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,14 @@
 import {
 	PRIVATE_ROUTES,
-	PUBLIC_ROUTES,
 	REDIRECT_WHEN_AUTHENTICATED,
 	REDIRECT_WHEN_NOT_AUTHENTICATED,
 } from "@/constants/routes";
 import { type MiddlewareConfig, type NextRequest, NextResponse } from "next/server";
-import { jwtDecode } from "jwt-decode";
 
 export function middleware(req: NextRequest) {
-	console.log("middleware activated");
-
 	const currPath = req.nextUrl.pathname;
 	const redirectUrl = req.nextUrl.clone();
 	const token = req.cookies.get("session");
-	if (token !== undefined) console.log(jwtDecode(token.value));
 
 	if (currPath === "/" && !token) {
 		return NextResponse.redirect(redirectUrl.origin + REDIRECT_WHEN_NOT_AUTHENTICATED);
