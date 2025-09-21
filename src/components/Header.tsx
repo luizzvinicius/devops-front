629cbf3a4d57827015342391d8f52d99e2002e91
luiz <viniciusadr244@gmail.com>
2025-09-21 11:34:03 -0300

    refactor: change nav color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,15 @@ import Link from "next/link";
 export function Header() {
 	return (
 		<nav className="flex self-start w-full p-4 gap-2">
-			<Link href="/pessoa" className="hover:underline hover:text-blue-400">
+			<Link href="/pessoa" className="text-custom hover:underline hover:text-blue-400">
 				Pessoa
 			</Link>
-			<p className="text-gray-500">|</p>
-			<Link href="/conta" className="hover:underline hover:text-blue-400">
+			<p className="text-custom">|</p>
+			<Link href="/conta" className="text-custom hover:underline hover:text-blue-400">
 				Conta
 			</Link>
-			<p className="text-gray-500">|</p>
-			<Link href="/movimentacao" className="hover:underline hover:text-blue-400">
+			<p className="text-custom">|</p>
+			<Link href="/movimentacao" className="text-custom hover:underline hover:text-blue-400">
 				Movimentação
 			</Link>
 		</nav>
