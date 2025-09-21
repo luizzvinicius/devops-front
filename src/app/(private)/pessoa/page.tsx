555288b3a1c83d0b66d6042de89a754f2ade427d
luiz <viniciusadr244@gmail.com>
2025-09-21 11:36:37 -0300

    refactor: change pessoa page style

--- src/app/(private)/pessoa/page.tsx
+++ src/app/(private)/pessoa/page.tsx
@@ -3,7 +3,7 @@ import { CreatePessoaForm } from "./-components/form/PessoaForm";
 export default function Pessoa() {
 	return (
 		<div className="min-w-full">
-			<h1 className="text-3xl text-center font-bold">Cadastro de Pessoa</h1>
+			<h1 className="text-3xl text-center font-bold text-custom">Cadastro de Pessoa</h1>
 			<CreatePessoaForm />
 		</div>
 	);
