a5dcb2fbed3aaea52d5e75cc5a9f1fe10bc59b63
luiz <viniciusadr244@gmail.com>
2025-10-03 18:59:51 -0300

    feat: update Profile component to display preferred username instead of name

--- src/app/(private)/_components/Profile.tsx
+++ src/app/(private)/_components/Profile.tsx
@@ -11,10 +11,10 @@ export default async function Profile() {
 		<div className="flex items-center gap-4 shadow-md rounded-2xl px-4 bg-[#f7f9fb]">
 			<div className="h-8 w-8 flex justify-center items-center rounded-[50%] bg-background-secondary">
 				<span className="text-custom">
-					{decodedToken.name.charAt(0).toLocaleUpperCase()}
+					{decodedToken.preferred_username.charAt(0).toLocaleUpperCase()}
 				</span>
 			</div>
-			<span className="text-sm">{decodedToken.name}</span>
+			<span className="text-sm">{decodedToken.preferred_username}</span>
 		</div>
 	);
 }
