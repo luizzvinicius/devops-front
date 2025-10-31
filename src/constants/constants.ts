0744698f255f6f3e0c30c7c350d870c16979ac35
luiz <viniciusadr244@gmail.com>
2025-10-31 18:24:43 -0300

    update parameters to build

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
 export const { DEV_MODE = process.env.NODE_ENV === "development" } = process.env;
 
-export const BASE_URL = DEV_MODE ? "http://localhost:8080/api/v1" : "http://backend:8080/api/v1";
+export const BASE_URL = "http://backend:8080/api/v1";
 export const AUTH_TOKEN = "session";
