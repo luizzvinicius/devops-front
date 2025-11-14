a4a58cb705618ce47432174d77543cc6cd1f4e54
luiz <viniciusadr244@gmail.com>
2025-11-14 00:51:54 -0300

    feat: add investimento route

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -7,4 +7,5 @@ export const PRIVATE_ROUTES = [
 	{ path: "/pessoa", action: "next" },
 	{ path: "/conta", action: "next" },
 	{ path: "/movimentacao", action: "next" },
+	{ path: "/investimentos", action: "next" },
 ] as const;
